d0e035ee0e24f1d32e2f7a4a3ec6c654f4365323
Natalia Vera <nvera@coopebombas.com>
2022-09-02 08:13:35 -0500

    Primer commit

---
+++ src/main/java/com/fundamentos/sprigboot/fundamentos/FundamentosApplication.java
@@ -0,0 +1,14 @@
+package com.fundamentos.sprigboot.fundamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosApplication {
+
+	public static void main(String[] args) {
+		//Inicializa toda la arquitectura y la  configuracion del proyecto
+		SpringApplication.run(FundamentosApplication.class, args);
+	}
+
+}
